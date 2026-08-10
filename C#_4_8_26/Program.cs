c543124a4a21f6977b8f297abf26d2fc5c43df0f
sourav dwivedi <souravdwivedi999@gmail.com>
2026-08-10 11:33:32 +0530

    Read only modifier

--- C#_4_8_26/Program.cs
+++ C#_4_8_26/Program.cs
@@ -7,16 +7,48 @@ namespace C__4_8_26
     {
         static void Main(string[] args)
         {
-            //var point = new Point(10, 20);
-            //point.Move(30, 40);
-            //Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
 
-            //point.Move(100, 200);
-            //Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            Console.WriteLine("Customer: Orders: {0}", customer.Orders.Count);
 
-            var calculator = new Calculator();
-            var result = calculator.Add(1, 2, 3, 4, 5);
-            Console.WriteLine("Addition result: {0}", result);
+            customer.NewOrder();
+
+
+            Console.WriteLine("Customer: Orders: {0}", customer.Orders.Count);
+        }
+
+        static void UseCalculator()
+        {
+            try
+            {
+                var calculator = new Calculator();
+                var result = calculator.Add(1, 2, 3, 4, 5);
+                Console.WriteLine("Addition result: {0}", result);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(30, 40);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
     }
