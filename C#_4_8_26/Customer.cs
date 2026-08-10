8c31dc7d749a50a2c38c094a73c7355faa42c48d
sourav dwivedi <souravdwivedi999@gmail.com>
2026-08-10 09:48:08 +0530

    Initial Commit

---
+++ C#_4_8_26/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__4_8_26
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public 
+
+        public Customer()
+        {
+            
+        }
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+
+}
---
+++ C#_4_8_26/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C__4_8_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1, "John Doe");
+            Console.WriteLine(customer.Name);
+            Console.WriteLine(customer.Id);
+        }    
+    }
+}
